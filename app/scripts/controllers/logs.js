575c7f9a51835f6e438479d48d651297b7c76e48
Dewey Hong <doohee323@gmail.com>
2015-03-09 09:43:50 -0700

    refresh resource

--- app/scripts/controllers/logs.js
+++ app/scripts/controllers/logs.js
@@ -13,7 +13,6 @@ angular.module('concordchurchApp')
 		$scope.$location = $location;
 		var read_at = $stateParams.read_at;
   	var prefix = "/api/bunch/v2/me/";
-debugger;
 		var today = moment().startOf('day').utc().toDate().format('YYYY-MM-DD');
 		if(!read_at) read_at = today;
 		
@@ -41,7 +40,7 @@ debugger;
 			$scope.retrieve(read_at);
 		}
 		
-	  $scope.refresh = function(read_at) {
+	  $scope.retrieve = function(read_at) {
 			if(!read_at) {
 				read_at = today;
 			}
@@ -49,30 +48,29 @@ debugger;
 				if(data.rows) {
 					$scope.logs = data.rows;
 					localStorage.setItem(prefix + read_at, JSON.stringify($scope.logs));
-					if(Android) {
-						Android.cacheJson(JSON.stringify($scope.logs));
-					}
+					//if(Android) {
+					//	Android.cacheJson(JSON.stringify($scope.logs));
+					//}
 			    config.curitem = $scope.logs[0];
 				}
 			}, function(error) {
-		   $scope.logs = [{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570",
-		   "title":"하나님이 세상을 이처럼 사랑하사","content":"요3:14-16","speaker":"길영환 목사",
-		   "date":"2014-12-21","bible":"[요] 3:14 모세가 광야에서 뱀을 든 것같이 인자도 들려야 하리니 [요] 3:15 이는 저를 믿는 자마...",
-		   "video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1",
-		   "desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"그를 믿는 자마다 멸망하지 않고","content":"요3:16-21","speaker":"길영환 목사","date":"2014-12-14","bible":"[요] 3:16 하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니 이는 저를 믿는 자마��� 멸망치 ...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"영생(Eternal Life)","content":"요5:24","speaker":"길영환 목사","date":"2014-12-28","bible":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"다니엘의 감사","content":"단6:10","speaker":"길영환 목사","date":"2014-11-09","bible":"[단] 6:10 다니엘이 이 조서에 어인이 찍힌 것을 알고도 자기 집에 돌아가서는 그 방의 예루살렘...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"}];
+				var datast = localStorage.getItem(prefix + read_at);
+				if(datast) {
+					$scope.logs = JSON.parse(datast);
+			    config.curitem = $scope.logs[0];
+			  }
 			});
 		}
 	
-	  $scope.retrieve = function(read_at) {
-			var datast = localStorage.getItem(prefix + read_at);
-			if(datast) {
-				$scope.logs = JSON.parse(datast);
-		    config.curitem = $scope.logs[0];
-			} else {
-				$scope.refresh(read_at);
-		  }
-		}
-    
+	  $scope.refresh = function() {
+			if(Android) {
+				if(Android.refresh()) {
+					alert(document.location.origin);
+					document.location.href = document.location.origin;
+				}
+			}
+		}	
+	
     $scope.open = function(item) {
     	config.item = item;
     	$state.go('video');
--- app/scripts/controllers/swipe.js
+++ app/scripts/controllers/swipe.js
@@ -22,7 +22,7 @@ angular.module('concordchurchApp')
 		$scope.retrieve(id);
 	}
 	
-  $scope.refresh = function(id) {
+  $scope.retrieve = function(id) {
 		if(!id) {
 			id = currentRow;
 		}
@@ -36,23 +36,22 @@ angular.module('concordchurchApp')
 		    config.curitem = $scope.words[0];
 			}
 		}, function(error) {
-	   $scope.words = [{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570",
-	   "title":"하나님이 세상을 이처럼 사랑하사","content":"요3:14-16","speaker":"길영환 목사",
-	   "date":"2014-12-21","bible":"[요] 3:14 모세가 광야에서 뱀을 든 것같이 인자도 들려야 하리니 [요] 3:15 이는 저를 믿는 자마...",
-	   "video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1",
-	   "desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"그를 믿는 자마다 멸망하지 않고","content":"요3:16-21","speaker":"길영환 목사","date":"2014-12-14","bible":"[요] 3:16 하나님이 세상을 이처럼 사랑하사 독생자를 주셨으니 이는 저를 믿는 자마��� 멸망치 ...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"영생(Eternal Life)","content":"요5:24","speaker":"길영환 목사","date":"2014-12-28","bible":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"},{"img":"http://ckbch.org/_data/ckbch/file/m2_1/thumb/470570","link":"http://ckbch.org/_chboard/bcast/audio.php?bo_table=m2_1&ty=v&wr_id=470570","title":"다니엘의 감사","content":"단6:10","speaker":"길영환 목사","date":"2014-11-09","bible":"[단] 6:10 다니엘이 이 조서에 어인이 찍힌 것을 알고도 자기 집에 돌아가서는 그 방의 예루살렘...","video":"http://www.youtube.com/v/ghS_TOQJ4ow?version=3&hl=en_US&rel=0&autoplay=1","desc":"[요] 5:24 내가 진실로 진실로 너희에게 이르노니 내 말을 듣고 또 나 보내신 이를 믿는 자는 영생을 얻었고 심판에 이르지 아니하나니 사망에서 생명으로 옮겼느니라아멘"}];
+			var datast = localStorage.getItem(prefix + id);
+			if(datast) {
+				$scope.words = JSON.parse(datast);
+		    config.curitem = $scope.words[0];
+		  }
 		});
 	}
 
-  $scope.retrieve = function(id) {
-		var datast = localStorage.getItem(prefix + id);
-		if(datast) {
-			$scope.words = JSON.parse(datast);
-	    config.curitem = $scope.words[0];
-		} else {
-			$scope.refresh(id);
-	  }
-	}
+  $scope.refresh = function() {
+		if(Android) {
+			if(Android.refresh()) {
+				alert(document.location.origin);
+				document.location.href = document.location.origin;
+			}
+		}
+	}	
 
   $scope.open = function(item) {
   	config.item = item;
